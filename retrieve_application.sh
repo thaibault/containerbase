291b80d07351227f6012749a9570cd8184cf1b7e
torben <t.sickert[~at~]gmail.com>
2018-04-13 13:12:32 +0200

    Automatic update to new version.

--- retrieve_application.sh
+++ retrieve_application.sh
@@ -13,33 +13,38 @@
 # NOTE: Current version of the application will live in the image. For
 # development scenarios we can simply mount our working copy over the
 # application root.
-touch "$INITIALIZING_FILE_PATH" && \
-chown "${MAIN_USER_NAME}:${MAIN_USER_GROUP_NAME}" \
-    "$INITIALIZING_FILE_PATH"
-[ "$STANDALONE" = true ] && \
-[[ "$PRIVATE_SSH_KEY" != '' ]] && \
-[[ "$PUBLIC_SSH_KEY" != '' ]] && \
-[[ "$REPOSITORY_URL" != '' ]] && \
-cd && \
-mkdir --parents .ssh && \
-echo -e "$PRIVATE_SSH_KEY" >.ssh/id_rsa && \
-chmod 600 .ssh/id_rsa && \
-echo -e "$PUBLIC_SSH_KEY" >.ssh/id_rsa.pub && \
-chmod 600 .ssh/id_rsa.pub && \
-echo -e "$KNOWN_HOSTS" >.ssh/known_hosts && \
-chmod 600 .ssh/known_hosts && \
-git clone --depth 1 --no-single-branch "$REPOSITORY_URL" \
-    "$APPLICATION_PATH" && \
-mkdir --parents "$APPLICATION_PATH" && \
-cd "$APPLICATION_PATH" && \
-git submodule init && \
-git submodule foreach \
-    'branch="$(git config --file "$toplevel/.gitmodules" "submodule.$name.branch")";git clone --depth 1 --branch "$branch"' && \
-git submodule update --remote && \
-rm --recursive --force .git && \
-chown "${MAIN_USER_NAME}:${MAIN_USER_GROUP_NAME}" \
-    "$APPLICATION_PATH" || \
-true
+for path in \
+    "$APPLICATION_USER_ID_INDICATOR_FILE_PATH" "$INITIALIZING_FILE_PATH"
+do
+    touch  && chown "${MAIN_USER_NAME}:${MAIN_USER_GROUP_NAME}" "$path"
+done
+if \
+    [ "$STANDALONE" = true ] && \
+    [[ "$PRIVATE_SSH_KEY" != '' ]] && \
+    [[ "$PUBLIC_SSH_KEY" != '' ]] && \
+    [[ "$REPOSITORY_URL" != '' ]]
+then
+    cd && \
+    mkdir --parents .ssh && \
+    echo -e "$PRIVATE_SSH_KEY" >.ssh/id_rsa && \
+    chmod 600 .ssh/id_rsa && \
+    echo -e "$PUBLIC_SSH_KEY" >.ssh/id_rsa.pub && \
+    chmod 600 .ssh/id_rsa.pub && \
+    echo -e "$KNOWN_HOSTS" >.ssh/known_hosts && \
+    chmod 600 .ssh/known_hosts && \
+    rm --force --recursive "$APPLICATION_PATH" &>/dev/null
+    git clone --depth 1 --no-single-branch "$REPOSITORY_URL" \
+        "$APPLICATION_PATH" && \
+    mkdir --parents "$APPLICATION_PATH" && \
+    cd "$APPLICATION_PATH" && \
+    git submodule init && \
+    git submodule foreach \
+        'branch="$(git config --file "$toplevel/.gitmodules" "submodule.$name.branch")";git clone --depth 1 --branch "$branch"' && \
+    git submodule update --remote && \
+    rm --recursive --force .git && \
+    chown "${MAIN_USER_NAME}:${MAIN_USER_GROUP_NAME}" \
+        "$APPLICATION_PATH"
+fi
 # region vim modline
 # vim: set tabstop=4 shiftwidth=4 expandtab:
 # vim: foldmethod=marker foldmarker=region,endregion:
