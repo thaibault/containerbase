ce6a047adcc83ed463d4eb433fbd639aff3b35d9
torben <t.sickert[~at~]gmail.com>
2019-09-16 12:18:07 +0200

    a

---
+++ prepare-initializer.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/bash
+# -*- coding: utf-8 -*-
+# region choose initializer script
+# We prefer the local mounted working copy managed initializer if available.
+if [[ "$1" != '--no-check-local-initializer' ]]; then
+    for file_path in \
+        "${APPLICATION_PATH}serviceHandler/initialize.sh" \
+        "${APPLICATION_PATH}initialize.sh"
+    do
+        if [ -s "$file_path" ]; then
+            exec "$file_path" --no-check-local-initializer
+        fi
+    done
+fi
+# endregion
+# region decrypt security related artefacts needed at runtime
+if \
+    [[ "$DECRYPT" != false ]] && \
+    [ -d "$ENCRYPTED_PATH" ] && \
+    [ -d "$DECRYPTED_PATH" ]
+then
+    if [ -s "$PASSWORD_FILE_PATH" ]; then
+        gocryptfs \
+            -extpass "cat '$PASSWORD_FILE_PATH'" \
+            "$ENCRYPTED_PATH" \
+            "$DECRYPTED_PATH"
+    else
+        gocryptfs "$ENCRYPTED_PATH" "$DECRYPTED_PATH"
+    fi
+fi
+# endregion
+# region vim modline
+# vim: set tabstop=4 shiftwidth=4 expandtab:
+# vim: foldmethod=marker foldmarker=region,endregion:
+# endregion
