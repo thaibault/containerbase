970e6b16c0f152152abfedd1ce902d8969f2930a
torben <t.sickert[~at~]gmail.com>
2022-02-21 09:03:33 +0100

    Automatic update to new version.

---
+++ initialize.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/bash
+# -*- coding: utf-8 -*-
+# region header
+# [Project page](https://torben.website/containerbase)
+
+# Copyright Torben Sickert (info["~at~"]torben.website) 16.12.2012
+
+# License
+# -------
+
+# This library written by Torben Sickert stand under a creative commons naming
+# 3.0 unported license.
+# See https://creativecommons.org/licenses/by/3.0/deed.de
+# endregion
+# shellcheck disable=SC2016,SC2034,SC2155
+source prepare-initializer "$@" && \
+
+set -e
+
+source decrypt "$@"
+
+source configure-runtime-user
+
+source run-command "$@"
+# region vim modline
+# vim: set tabstop=4 shiftwidth=4 expandtab:
+# vim: foldmethod=marker foldmarker=region,endregion:
+# endregion
