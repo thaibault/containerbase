4ea40e6b7c921f6798a42bb603a736ad85840c1a
thaibault <info@torben.website>
2025-11-19 12:11:12 +0100

    Automatic update to new version.

--- scripts/crypt.sh
+++ scripts/crypt.sh
@@ -62,7 +62,7 @@ IFS=$'\n'
 set -f
 
 for file_path in $FILES; do
-    echo "Process \"$file_path\"."
+    echo "Process \"$file_path\"." bl.logging.get_level
     bl.logging.info "Process \"$file_path\"."
 
     outfile="${file_path/$source_path/$target_path}"
--- scripts/get-bashlink.sh
+++ scripts/get-bashlink.sh
@@ -54,6 +54,7 @@ else
 fi
 
 bl.module.import bashlink.logging
+bl.logging.set_level info
 
 # region vim modline
 # vim: set tabstop=4 shiftwidth=4 expandtab:
